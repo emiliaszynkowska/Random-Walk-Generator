273d8178ddf4bf9d79e22210360fac5566fe8f53
Emilia <42769841+emiliaszynkowska@users.noreply.github.com>
2020-10-17 11:29:13 +0100

    Uploading Files

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,280 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    //Tiles
+    public Tile groundTile;
+    public Tile wallTile;
+    public Tile edgeTile;
+    public Tile leftEdgeTile;
+    public Tile rightEdgeTile;
+    public Tile leftCornerTile;
+    public Tile rightCornerTile;
+    public Tile leftPointTile;
+    public Tile rightPointTile;
+    public Tile leftWallTile;
+    public Tile rightWallTile;
+    //Tilemaps
+    public Tilemap groundMap;
+    public Tilemap outerWallMap;
+    public Tilemap innerWallMap;
+    public Tilemap edgeMap;
+    //Variables
+    public int deviationRate;
+    public int roomRate;
+    public int maxRouteLength;
+    public int maxRoutes;
+    public int routeCount;
+
+    private void Start()
+    {
+        int x = 0;
+        int y = 0;
+        int routeLength = 0;
+        
+        GenerateSquare(x, y, 1);
+        Vector2Int previousPos = new Vector2Int(x, y);
+        y += 3;
+        GenerateSquare(x, y, 1);
+        
+        NewRoute(x, y, routeLength, previousPos);
+        FillTiles();
+    }
+
+    private void FillTiles()
+    {
+        BoundsInt bounds = groundMap.cellBounds;
+        for (int xMap = bounds.xMin - 10; xMap <= bounds.xMax + 10; xMap++)
+        {
+            for (int yMap = bounds.yMin - 10; yMap <= bounds.yMax + 10; yMap++)
+            {
+                Vector3Int pos = new Vector3Int(xMap, yMap, 0);
+                Vector3Int posLower = new Vector3Int(xMap, yMap - 1, 0);
+                Vector3Int posUpper = new Vector3Int(xMap, yMap + 1, 0);
+
+                if (groundMap.GetTile(pos) == null)
+                {
+                    //If it is a top wall
+                    if (groundMap.GetTile(posLower) != null)
+                    {
+                        //Fill in a wall and edge
+                        outerWallMap.SetTile(pos,wallTile);
+                        edgeMap.SetTile(posUpper,edgeTile);
+                        
+                    }
+                    //If it is a bottom wall
+                    else if (groundMap.GetTile(posUpper) != null)
+                    {
+                        //Fill in a wall and edge
+                        outerWallMap.SetTile(pos,wallTile);
+                        edgeMap.SetTile(posUpper,edgeTile);
+                    }
+                }
+            }
+        }
+        
+        for (int xMap = bounds.xMin - 10; xMap <= bounds.xMax + 10; xMap++)
+        {
+            for (int yMap = bounds.yMin - 10; yMap <= bounds.yMax + 10; yMap++)
+            {
+                Vector3Int pos = new Vector3Int(xMap, yMap, 0);
+                Vector3Int posLower = new Vector3Int(xMap, yMap - 1, 0);
+                Vector3Int posUpper = new Vector3Int(xMap, yMap + 1, 0);
+                Vector3Int posLeft = new Vector3Int(xMap - 1, yMap, 0);
+                Vector3Int posRight = new Vector3Int(xMap + 1, yMap, 0);
+                Vector3Int posUpperLeft = new Vector3Int(xMap - 1, yMap + 1, 0);
+                Vector3Int posUpperRight = new Vector3Int(xMap + 1, yMap + 1, 0);
+                Vector3Int posLowerLeft = new Vector3Int(xMap - 1, yMap - 1, 0);
+                Vector3Int posLowerRight = new Vector3Int(xMap + 1, yMap - 1, 0);
+
+                if (groundMap.GetTile(pos) != null)
+                {
+                    //It it is a left edge
+                    if (groundMap.GetTile(posLeft) == null && outerWallMap.GetTile(posLeft) == null)
+                    {
+                        //If it is a top left corner
+                        if (outerWallMap.GetTile(posUpper) != null)
+                        {
+                            //Fill in a top left corner
+                            edgeMap.SetTile(pos,leftEdgeTile);
+                            edgeMap.SetTile(posUpper,leftEdgeTile);
+                        }
+                        //If it is a bottom left corner
+                        else if (outerWallMap.GetTile(posLower) != null)
+                        {
+                            //Fill in a bottom left corner
+                            edgeMap.SetTile(pos,leftCornerTile);
+                        }
+                        else
+                        {
+                            //Fill in a left edge
+                            edgeMap.SetTile(pos,leftEdgeTile);
+                        }
+                    }
+                    
+                    //If it is a right edge
+                    if (groundMap.GetTile(posRight) == null && outerWallMap.GetTile(posRight) == null)
+                    {
+                        //If it is a top right corner
+                        if (outerWallMap.GetTile(posUpper) != null)
+                        {
+                            //Fill in a top right corner
+                            edgeMap.SetTile(pos,rightEdgeTile);
+                            edgeMap.SetTile(posUpper,rightEdgeTile);
+                        }
+                        //If it is a bottom right corner
+                        else if (outerWallMap.GetTile(posLower) != null)
+                        {
+                            //Fill in a bottom right corner
+                            edgeMap.SetTile(pos,rightCornerTile);
+                        }
+                        else
+                        {
+                            //Fill in a right edge
+                            edgeMap.SetTile(pos,rightEdgeTile);
+                        }
+                    }
+                    
+                    //If a left edge appears by a wall
+                    if (outerWallMap.GetTile(posLeft) != null && groundMap.GetTile(posRight) != null && groundMap.GetTile(posUpper) != null && groundMap.GetTile(posLowerLeft) == null)
+                        edgeMap.SetTile(pos,leftEdgeTile);
+                    //If a right edge appears by a wall
+                    if (outerWallMap.GetTile(posRight) != null && groundMap.GetTile(posLeft) != null && groundMap.GetTile(posUpper) != null && groundMap.GetTile(posLowerRight) == null)
+                        edgeMap.SetTile(pos,rightEdgeTile);
+                    
+                    //If two walls appear diagonally
+                    if (outerWallMap.GetTile(posLeft) != null && outerWallMap.GetTile(posUpper) != null)
+                    {
+                        edgeMap.SetTile(posUpperLeft, rightCornerTile);
+                        edgeMap.SetTile(new Vector3Int(xMap - 1, yMap + 2, 0),leftPointTile);
+                    }
+
+                    if (outerWallMap.GetTile(posRight) != null && outerWallMap.GetTile(posUpper) != null)
+                    {
+                        edgeMap.SetTile(posUpperRight, leftCornerTile);
+                        edgeMap.SetTile(new Vector3Int(xMap + 1, yMap + 2, 0),rightPointTile);
+                    }
+
+                    if (outerWallMap.GetTile(posLeft) != null && outerWallMap.GetTile(posLower) != null)
+                    {
+                        edgeMap.SetTile(pos,leftCornerTile);
+                        edgeMap.SetTile(posUpper,rightPointTile);
+                    }
+                    if (outerWallMap.GetTile(posRight) != null && outerWallMap.GetTile(posLower) != null)
+                    {
+                        edgeMap.SetTile(pos,rightCornerTile);
+                        edgeMap.SetTile(posUpper,leftPointTile);
+                    }
+                }
+
+                if (outerWallMap.GetTile(pos) != null)
+                {
+                    //If a wall comes to a point
+                    if (groundMap.GetTile(posLeft) != null && groundMap.GetTile(posUpper) != null)
+                        edgeMap.SetTile(posUpperLeft,leftPointTile);
+                    if (groundMap.GetTile(posRight) != null && groundMap.GetTile(posUpper) != null)
+                        edgeMap.SetTile(posUpperRight,rightPointTile);
+                    
+                    //If a wall extends with the edge
+                    if (groundMap.GetTile(posLeft) != null && edgeMap.GetTile(posLeft) == null && edgeMap.GetTile(posUpper) != null && outerWallMap.GetTile(posUpperLeft) == null)
+                    {
+                        innerWallMap.SetTile(posLeft,leftWallTile);
+                    }
+                    if (groundMap.GetTile(posRight) != null && edgeMap.GetTile(posRight) == null && edgeMap.GetTile(posUpper) != null && outerWallMap.GetTile(posUpperRight) == null)
+                    {
+                        innerWallMap.SetTile(posRight,rightWallTile);
+                    }
+                }
+            }
+        }
+    }
+
+    private void NewRoute(int x, int y, int routeLength, Vector2Int previousPos)
+    {
+        if (routeCount < maxRoutes)
+        {
+            routeCount++;
+            while (++routeLength < maxRouteLength)
+            {
+                //Initialize
+                bool routeUsed = false;
+                int xOffset = x - previousPos.x; 
+                int yOffset = y - previousPos.y; 
+                int roomSize = 1; 
+                if (Random.Range(1, 100) <= roomRate)
+                    roomSize = Random.Range(3, 6);
+                previousPos = new Vector2Int(x, y);
+
+                //Go Straight
+                if (Random.Range(1, 100) <= deviationRate)
+                {
+                    if (routeUsed)
+                    {
+                        GenerateSquare(previousPos.x + xOffset, previousPos.y + yOffset, roomSize);
+                        NewRoute(previousPos.x + xOffset, previousPos.y + yOffset, Random.Range(routeLength, maxRouteLength), previousPos);
+                    }
+                    else
+                    {
+                        x = previousPos.x + xOffset;
+                        y = previousPos.y + yOffset;
+                        GenerateSquare(x, y, roomSize);
+                        routeUsed = true;
+                    }
+                }
+
+                //Go left
+                if (Random.Range(1, 100) <= deviationRate)
+                {
+                    if (routeUsed)
+                    {
+                        GenerateSquare(previousPos.x - yOffset, previousPos.y + xOffset, roomSize);
+                        NewRoute(previousPos.x - yOffset, previousPos.y + xOffset, Random.Range(routeLength, maxRouteLength), previousPos);
+                    }
+                    else
+                    {
+                        y = previousPos.y + xOffset;
+                        x = previousPos.x - yOffset;
+                        GenerateSquare(x, y, roomSize);
+                        routeUsed = true;
+                    }
+                }
+                //Go right
+                if (Random.Range(1, 100) <= deviationRate)
+                {
+                    if (routeUsed)
+                    {
+                        GenerateSquare(previousPos.x + yOffset, previousPos.y - xOffset, roomSize);
+                        NewRoute(previousPos.x + yOffset, previousPos.y - xOffset, Random.Range(routeLength, maxRouteLength), previousPos);
+                    }
+                    else
+                    {
+                        y = previousPos.y - xOffset;
+                        x = previousPos.x + yOffset;
+                        GenerateSquare(x, y, roomSize);
+                        routeUsed = true;
+                    }
+                }
+
+                if (!routeUsed)
+                {
+                    x = previousPos.x + xOffset;
+                    y = previousPos.y + yOffset;
+                    GenerateSquare(x, y, roomSize);
+                }
+            }
+        }
+    }
+
+    private void GenerateSquare(int x, int y, int radius)
+    {
+        for (int tileX = x - radius; tileX <= x + radius; tileX++)
+        {
+            for (int tileY = y - radius; tileY <= y + radius; tileY++)
+            {
+                Vector3Int tilePos = new Vector3Int(tileX, tileY, 0);
+                groundMap.SetTile(tilePos, groundTile);
+            }
+        }
+    }
+}
